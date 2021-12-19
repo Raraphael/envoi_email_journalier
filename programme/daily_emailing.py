7d77f1d7e23e9fb87b6dc2800008ec75380d315c
Raphael Bitoun <raphael.bitoun37@gmail.com>
2021-12-19 23:58:45 +0100

    black typo

--- programme/daily_emailing.py
+++ programme/daily_emailing.py
@@ -8,7 +8,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 import datetime
-import os 
+import os
 import re
 import sys
 import io
@@ -17,10 +17,12 @@ import io
 packet = io.BytesIO()
 # create a new PDF with Reportlab
 can = canvas.Canvas(packet, pagesize=letter)
-ddj_write  = datetime.date.today().strftime('%d    %m      %Y')        #spaces to fit in the pdf boxes
-can.drawString(365, 632, ddj_write)     
+ddj_write = datetime.date.today().strftime(
+    "%d    %m      %Y"
+)  # spaces to fit in the pdf boxes
+can.drawString(365, 632, ddj_write)
 can.save()
-#move to the beginning of the StringIO buffer
+# move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
@@ -31,60 +33,68 @@ page = existing_pdf.getPage(0)
 page.mergePage(new_pdf.getPage(0))
 output.addPage(page)
 # finally, write "output" to a real file
-ddj_formate  = datetime.date.today().strftime('%d_%m')                                  #récupère la date du jour sous un autre format pour l'insérer dans le nom du document
-outputStream = open("raphael_bitoun_emargement_Data_analyst_"+ddj_formate+".pdf", "wb")
+ddj_formate = datetime.date.today().strftime(
+    "%d_%m"
+)  # récupère la date du jour sous un autre format pour l'insérer dans le nom du document
+outputStream = open(
+    "raphael_bitoun_emargement_Data_analyst_" + ddj_formate + ".pdf", "wb"
+)
 output.write(outputStream)
 outputStream.close()
 
 print("la récupération du fichier s'est faite avec succès")
 #### PART 2 SEND EMAIL#####
 
-#Date du jour 
-ddj  = datetime.date.today().strftime('%d/%m/%Y')
+# Date du jour
+ddj = datetime.date.today().strftime("%d/%m/%Y")
 
 # Email adress
-email_user = "37raphael.bitoun@gmail.com"
+email_user = ""
+
+# Email_pwd
+email_pwd = ""
 
 # reception email  --> xxxxx@cefim.eu
-email_destination = 
+email_destination = ""
 
-#Subject of the message
-Subject = 'Emargement Rbitoun data analyst '+ddj
+# Subject of the message
+Subject = "Emargement Rbitoun data analyst " + ddj
 
 
-filename = 'emargement.pdf'
+filename = "emargement.pdf"
 
 # message body
-body = "Bonjour Séverine\nci-joint mon émargement du {}\nBonne Journée\nBien à toi\nRaphaël Bitoun".format(ddj)
+body = "Bonjour Séverine\nci-joint mon émargement du {}\nBonne Journée\nBien à toi\nRaphaël Bitoun".format(
+    ddj
+)
 
-#Retrieving the pdf at the root of the script
+# Retrieving the pdf at the root of the script
 path = os.getcwd()
 files = os.listdir(path)
-research = re.compile('.*\.pdf')
-#Attached document
+research = re.compile(".*\.pdf")
+# Attached document
 filename = (list(filter(research.match, files)))[0]
 
 # message instantiation
 msg = MIMEMultipart()
-msg['From'] = email_user
-msg['to'] = email_destination
-msg['subject'] = Subject
-msg.attach(MIMEText(body, 'plain'))
-attachment = open(filename, 'rb')
+msg["From"] = email_user
+msg["to"] = email_destination
+msg["subject"] = Subject
+msg.attach(MIMEText(body, "plain"))
+attachment = open(filename, "rb")
 
 # encoding document
-part = MIMEBase("application", 'octet-stream')
+part = MIMEBase("application", "octet-stream")
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= "+filename)
+part.add_header("Content-Disposition", "attachment; filename= " + filename)
 
 msg.attach(part)
 text = msg.as_string()
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
-server.login(email_user, <votre-mot-de-passe) 
+server.login(email_user, email_pwd)
 
-server.sendmail(email_user,email_destination, text)
+server.sendmail(email_user, email_destination, text)
 server.quit()
 print("l'envoi de l'email s'est bien effectué!")
-
